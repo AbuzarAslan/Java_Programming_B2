7a571c8e230769092c55c969670d65f86264a7be
abuza <abuzar.aslanov@gmail.com>
2023-11-09 19:07:17 -0500

    Constructor class. Nov 08, 2023

--- src/day26_methods/Painter.java
+++ src/day26_methods/Painter.java
@@ -1,33 +1,32 @@
 package day26_methods;
 
 
-    public class Painter {
-        public static void main(String[] args) {
+public class Painter {
+    public static void main(String[] args) {
 
-            Picture.draw();
-            Picture.draw();
-
-
-            Picture.draw("Green");
-
-            Picture.draw(5);
-
-            Picture.draw("Red", "Yellow");
-
-            Picture.draw( "Red", 6);
-
-            Picture.draw( "Red", 6);
-
-            Picture.draw( 6, "Black");
+        Picture.draw();
+        Picture.draw();
 
 
-            Picture.test(6.5);
+        Picture.draw("Green");
 
-            Picture.test(6);
+        Picture.draw(5);
+
+        Picture.draw("Red", "Yellow");
+
+        Picture.draw( "Red", 6);
+
+        Picture.draw( "Red", 6);
+
+        Picture.draw( 6, "Black");
 
 
+        Picture.test(6.5);
 
-        }
+        Picture.test(6);
+
+
 
     }
 
+}
